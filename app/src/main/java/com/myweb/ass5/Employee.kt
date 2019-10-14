2d8e27044fed0bc6fbbcfe1e7e194b5e430535fa
theerasak199 <54529697+theerasak199@users.noreply.github.com>
2019-10-14 22:45:45 +0700

    ASS5_SEC1_603020353-1

---
+++ app/src/main/java/com/myweb/ass5/Employee.kt
@@ -0,0 +1,8 @@
+package com.myweb.ass5
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Employee (val name: String, val gender: String, val email: String, val salary: Int) : Parcelable {
+}
---
+++ app/src/main/java/com/myweb/ass5/SecondActivity.kt
@@ -0,0 +1,23 @@
+package com.myweb.ass5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        var data =intent.extras
+        var newEmployee=data?.getParcelable<Employee>("empData")
+        nameText.text = "Employee Name : ${newEmployee?.name}"
+        genderText.text = "Gender : ${newEmployee?.gender}"
+        emailText.text = "E-mail : ${newEmployee?.email}"
+        salaryText.text = "Salary : ${newEmployee?.salary} Bath"
+    }
+    fun onClickClose(view: View){
+        finish()
+    }
+}
